1b1ec563905dbe66b28a71cdaeeb95caa07e572e
brsnb <49287229+brsnb@users.noreply.github.com>
2020-11-16 15:05:02 -0800

    .

--- src/render/device.rs
+++ src/render/device.rs
@@ -1,5 +1,11 @@
 use ash::vk;
 
+pub struct QueueFamilyIndices {
+    graphics: Option<u32>,
+    compute: Option<u32>,
+    transfer: Option<u32>,
+}
+
 pub struct Device {
     pub physical_device: vk::PhysicalDevice,
     pub logical_device: ash::Device,
@@ -9,6 +15,8 @@ pub struct Device {
     pub enabled_features: vk::PhysicalDeviceFeatures,
     pub supported_exts: Vec<String>,
     pub command_pool: vk::CommandPool,
+    pub queue_family_properties: Vec<vk::QueueFamilyProperties>,
+    pub queue_family_indicies: QueueFamilyIndices,
 }
 
 impl Device {
